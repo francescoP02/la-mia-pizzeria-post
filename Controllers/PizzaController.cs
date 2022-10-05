488681059249f061f5857701ee8ef4c46c8d2dff
francescoP02 <file.partipilo@gmail.com>
2022-10-05 12:48:49 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,77 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        List<Pizza> pizzasList = new List<Pizza>();
+
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> pizzasList = new List<Pizza>();
+
+            Pizza Margherita = new Pizza(0, "Margherita", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "pizza-margherita.jpg", "3,99");
+            Pizza Marinara = new Pizza(1, "Marinara", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "marinara.jpg", "3,00");
+            Pizza Mortadella = new Pizza(2, "Mortadella pistacchio e stracciatella", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "mortadella.jpg", "7,99");
+            Pizza QuattroFormaggi = new Pizza(3, "Quattro Formaggi", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "formaggi.jpg", "5,99");
+            Pizza Crudaiola = new Pizza(4, "Crudaiola", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "crudaiola.jpg", "5,99");
+
+
+            pizzasList.Add(Margherita);
+            pizzasList.Add(Marinara);
+            pizzasList.Add(Mortadella);
+            pizzasList.Add(QuattroFormaggi);
+            pizzasList.Add(Crudaiola);
+            
+
+
+            return View(pizzasList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Show(int id)
+        {
+            List<Pizza> pizzasList = new List<Pizza>();
+
+            Pizza Margherita = new Pizza(0, "Margherita", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "pizza-margherita.jpg", "3,99");
+            Pizza Marinara = new Pizza(1, "Marinara", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "marinara.jpg", "3,00");
+            Pizza Mortadella = new Pizza(2, "Mortadella pistacchio e stracciatella", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "mortadella.jpg", "7,99");
+            Pizza QuattroFormaggi = new Pizza(3, "Quattro Formaggi", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "formaggi.jpg", "5,99");
+            Pizza Crudaiola = new Pizza(4, "Crudaiola", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed", "crudaiola.jpg", "5,99");
+
+
+            pizzasList.Add(Margherita);
+            pizzasList.Add(Marinara);
+            pizzasList.Add(Mortadella);
+            pizzasList.Add(QuattroFormaggi);
+            pizzasList.Add(Crudaiola);
+
+            if (id >= pizzasList.Count)
+            {
+                return View("Error");
+            }
+
+            return View("Show", pizzasList[id]);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(int id, string name, string description, string photo, string price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Photo = photo;
+            Price = float.Parse(price);
+        }
+    }
+}
